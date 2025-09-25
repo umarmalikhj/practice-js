9ac7514171b3c9688262705b4a12c383b6eae5e3
Umar Malik <umarmalik.hj@gmail.com>
2025-09-25 07:38:21 +0530

    Learnt For Loops

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let a = 10;
-let b = 20;
+// let a = 10;
+// let b = 20;
 
-console.log("Value Of A (Before Swap) - ", a);
-console.log("Value Of B (Before Swap) - ", b);
+// console.log("Value Of A (Before Swap) - ", a);
+// console.log("Value Of B (Before Swap) - ", b);
 
-let temp = a;
-a = b;
-b = temp;
+// let temp = a;
+// a = b;
+// b = temp;
 
-console.log("Value Of A (After Swap) - ", a);
-console.log("Value Of B (After Swap) - ", b);
+// console.log("Value Of A (After Swap) - ", a);
+// console.log("Value Of B (After Swap) - ", b);
 
 // let n = Number(prompt("Enter Number: "));
 
@@ -21,11 +21,22 @@ console.log("Value Of B (After Swap) - ", b);
 //     console.log("The Number Entered Is Odd")
 // }
 
-let n = Number(prompt("Enter A Number: "));
+// let n = Number(prompt("Enter A Number: "));
+// let sum = 0;
+
+// for (let i=0; i<=n; i++){
+//     sum += i;
+// }
+
+// console.log(sum)
+
+let n = Number(prompt("Enter A Number"));
 let sum = 0;
 
-for (let i=0; i<=n; i++){
-    sum += i;
+for (let i=1; i<=n; i++){
+    if(i % 3 === 0 || i % 5 === 0){
+        sum += i;
+    }
 }
 
 console.log(sum)
