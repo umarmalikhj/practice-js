f68524eaf207b257324369fa0d5abf5daf0f5321
Umar Malik <umarmalik.hj@gmail.com>
2025-10-11 06:58:18 +0530

    Learnt Objects and Object Methods

--- script.js
+++ script.js
@@ -555,20 +555,55 @@
 //     console.log("Odd Number");
 // }
 
-const marks = Number(prompt("Enter Your Marks: "));
+// const marks = Number(prompt("Enter Your Marks: "));
 
-if (marks >= 90){
-    console.log("A");
-}
+// if (marks >= 90){
+//     console.log("A");
+// }
 
-else if (marks >= 75){
-    console.log("B");
-}
+// else if (marks >= 75){
+//     console.log("B");
+// }
 
-else if (marks >= 50){
-    console.log("C");
-}
+// else if (marks >= 50){
+//     console.log("C");
+// }
 
-else{
-    console.log("Fail");
+// else{
+//     console.log("Fail");
+// }
+
+const day = Number(prompt("Enter A Day (1 - 7)"));
+
+switch (day){
+    case 1 :
+        console.log("Monday");
+        break;
+    
+    case 2 :
+        console.log("Tuesday");
+        break;
+
+    case 3 : 
+        console.log("Wednesday");
+        break;
+
+    case 4 : 
+        console.log("Thursday");
+        break;
+
+    case 5 : 
+        console.log("Friday");
+        break;
+
+    case 6 : 
+        console.log("Saturday");
+        break;
+
+    case 7 :
+        console.log("Sunday");
+        break;
+
+    default : 
+        console.log("You've Entered Wrong One!")
 }
