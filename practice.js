fba63a862af172b35b007318624b6e72a6e7945d
Umar Malik <umarmalik.hj@gmail.com>
2025-12-25 11:46:05 +0530

    Added The Code

--- practice.js
+++ practice.js
@@ -5,6 +5,22 @@
 
 // console.log(age, name);
 
-for(let i = 1; i <= 5; i++){
-    console.log(i);
-}
+// for(let i = 1; i <= 5; i++){
+//     console.log(i);
+// }
+
+// console.log("Hello World");
+
+// let age = 21;
+// const name = "H";
+// const goal = "Become A Serious Developer";
+// let future;
+
+// age = 22;
+
+// console.log(age + "\n" + name + "\n" + goal + "\n" + future);
+
+let a = 10;
+let b = 3;
+
+console.log
