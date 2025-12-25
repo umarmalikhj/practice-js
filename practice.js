b4098c4721a0dc942f27391dc7305c4bf9aa0849
Umar Malik <umarmalik.hj@gmail.com>
2025-12-25 12:16:23 +0530

    Added The Code

--- practice.js
+++ practice.js
@@ -20,7 +20,62 @@
 
 // console.log(age + "\n" + name + "\n" + goal + "\n" + future);
 
-let a = 10;
-let b = 3;
+// let a = 10;
+// let b = 3;
 
-console.log
+// console.log("Arithmetic Operations");
+
+// console.log(a + b); //Addition
+// console.log(a - b); //Subtraction
+// console.log(a * b); //Multiplication
+// console.log(a / b); //Division
+// console.log(a % b); //Modulos
+
+// let score = 10;
+// score /= 2;
+// console.log(score);
+
+// console.log(5 !== "5");
+
+// isTrue = true;
+// isFalse = false;
+
+// console.log(isTrue && isFalse);
+// console.log(isTrue || isFalse);
+// console.log(isTrue && !isFalse);
+
+// let age = 17;
+
+// if(age >= 18){
+//     console.log("Adult");
+// }
+
+// else{
+//     console.log("Minor");
+// }
+
+const marks = Number(prompt("Enter The Marks"));
+
+if(isNaN(marks) || marks < 0 || marks > 100){
+    console.log("Invalid Marks");
+}
+
+else if (marks >= 85){
+    console.log("Distinction");
+}
+
+else if(marks >= 60){
+    console.log("First Class");
+}
+
+else if(marks >= 50){
+    console.log("Second Class");
+}
+
+else if(marks >= 35){
+    console.log("Pass");
+}
+
+else {
+    console.log("Fail");
+}
