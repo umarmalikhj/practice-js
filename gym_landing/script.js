597cbcadb878b1213e4a82a6e06aad22e33b4d76
umarmalikhj <umarmalik.hj@gmail.com>
2026-02-11 08:26:22 +0530

    Added Responsiveness

---
+++ gym_landing/script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("nav-links");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
