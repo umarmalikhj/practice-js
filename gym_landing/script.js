736cad339734bcc382af1bf8c437264f36f2d386
umarmalikhj <umarmalik.hj@gmail.com>
2026-02-15 07:02:07 +0530

    Did A Few Changes

--- gym_landing/script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("nav-links");
-    if (x.style.display === "flex") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "flex";
-    }
-}
