1e5b94c9b5605a0521fe939e6b37ed51574899f7
umarmalikhj <umarmalik.hj@gmail.com>
2026-04-05 07:27:42 +0530

    Added Code

--- practice.js
+++ practice.js
@@ -1638,21 +1638,30 @@
 //     para.innerText = "Click The Button!!!";
 // })
 
-let input = document.getElementById("text");
-let addBtn = document.getElementById("add");
-let taskList = document.getElementById("tasklist");
-let tasks = [];
+// let input = document.getElementById("text");
+// let addBtn = document.getElementById("add");
+// let taskList = document.getElementById("tasklist");
+// let tasks = [];
 
-addBtn.addEventListener("click", () => {
-    tasks.push(input.value);
-    taskList.innerHTML = "";
-    for (let i = 0; i < tasks.length; i++){
-        let newTask = document.createElement("li");
-        newTask.textContent = tasks[i];
-        taskList.append(newTask);
-        let deleteBtn = document.createElement("button");
-        deleteBtn.innerHTML = "Delete";
-        newTask.append(deleteBtn);
-    }
-    input.value = "";
-})
+// addBtn.addEventListener("click", () => {
+//     tasks.push(input.value);
+//     taskList.innerHTML = "";
+//     renderUI();
+//     input.value = "";
+// })
+
+// function renderUI() {
+//     taskList.innerHTML = "";
+//     for (let i = 0; i < tasks.length; i++) {
+//         let newTask = document.createElement("li");
+//         newTask.textContent = tasks[i];
+//         taskList.append(newTask);
+//         let deleteBtn = document.createElement("button");
+//         deleteBtn.innerHTML = "Delete";
+//         newTask.append(deleteBtn);
+//         deleteBtn.addEventListener("click", () => {
+//             tasks.splice(i, 1);
+//             renderUI();
+//         })
+//     }
+// }
