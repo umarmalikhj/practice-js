c71518783a0d7c5fea104c27028f8a869348e873
umarmalikhj <umarmalik.hj@gmail.com>
2026-04-05 07:29:39 +0530

    Added Code Of Script Using Vanilla JS

--- To-do_App/app.js
+++ To-do_App/app.js
@@ -1,19 +1,27 @@
-const input = document.getElementById('tasks');
-const addTask = document.getElementById('addTask');
-const taskList = document.getElementById('taskList');
+let input = document.getElementById("text");
+let addBtn = document.getElementById("add");
+let taskList = document.getElementById("tasklist");
+let tasks = [];
 
-addTask.addEventListener("click", () => {
-    task = input.value.trim();
-    if (!task) {
-        return;
+addBtn.addEventListener("click", () => {
+    tasks.push(input.value);
+    taskList.innerHTML = "";
+    renderUI();
+    input.value = "";
+})
+
+function renderUI() {
+    taskList.innerHTML = "";
+    for (let i = 0; i < tasks.length; i++) {
+        let newTask = document.createElement("li");
+        newTask.textContent = tasks[i];
+        taskList.append(newTask);
+        let deleteBtn = document.createElement("button");
+        deleteBtn.innerHTML = "Delete";
+        newTask.append(deleteBtn);
+        deleteBtn.addEventListener("click", () => {
+            tasks.splice(i, 1);
+            renderUI();
+        })
     }
-
-    else {
-        const li = document.createElement('li');
-        li.textContent = task;
-        taskList.appendChild(li);
-    }
-
-    input.value = '';
-});
-
+}
